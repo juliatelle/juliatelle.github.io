d636d8b403997f8be4ad44bf19aae0b92e54277b
juliatelle <juliatelle97@gmail.com>
2025-12-08 10:51:58 -0600

    Add files via upload

---
+++ webdevelopment/webdevelopment/header.php
@@ -0,0 +1,29 @@
+<body>
+  <!--Header-->
+ <header>
+  <img src="logo.jpg" alt="Logo" class="logo">
+
+  <!-- Mobile Hamburger Menu -->
+  <input type="checkbox" id="menu-toggle">
+  <label for="menu-toggle" class="hamburger">☰</label>
+
+  <!-- Navigation -->
+  <nav class="menu">
+    <ul>
+      <li><a href="index.html">Home</a></li>
+      <li><a href="about.html">About</a></li>
+
+      <li class="dropdown">
+        <a href="project.html">Content ▼</a>
+        <ul class="dropdown-content">
+          <li><a href="#canva">Canva</a></li>
+          <li><a href="#wevideo">WeVideo</a></li>
+          <li><a href="#miro">Miro</a></li>
+        </ul>
+      </li>
+
+      <li><a href="Resources.html">Resources</a></li>
+      <li><a href="contact.html">Contact</a></li>
+    </ul>
+  </nav>
+</header>
